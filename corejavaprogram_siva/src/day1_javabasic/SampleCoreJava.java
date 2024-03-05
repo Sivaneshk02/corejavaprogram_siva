f2135005a8bc1b1833651ba6c61bb53d56a65b4e
Dharun J <Dharun J@DHARUN>
2024-03-05 16:32:20 +0530

    it my addtion added to subraction

--- corejavaprogram_siva/src/day1_javabasic/SampleCoreJava.java
+++ corejavaprogram_siva/src/day1_javabasic/SampleCoreJava.java
@@ -15,6 +15,9 @@ public class SampleCoreJava {
         
         c=a+b;
         System.out.println("Addition of a="+a+" and b="+b+"is :"+c);
+        
+        c=a-b;
+        System.out.println("Subraction of a="+a+" and b="+b+"is :"+c);
 	}
 
 }
